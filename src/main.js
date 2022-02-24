e16f13a942c0e0ec8c3a265e0b549e6e837b1c05
9766x <xq.zyl@qq.com>
2022-02-24 11:31:56 +0800

    项目初始化-引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载全局样式
+import './styles/index.less'
+
+// import Vue from 'vue';
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
